f98525d63e1e2fd2be0ac639247ff9d71c109e6f
meelan7-7 <rajraijohn3132525@gmail.com>
2026-04-13 17:35:18 +0530

    Connect frontend with backend using axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api"
 });
 
-// 🔐 Attach token automatically to every request
+// 🔐 Attach token automatically to every request , Connect frontend with backend using axios
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
